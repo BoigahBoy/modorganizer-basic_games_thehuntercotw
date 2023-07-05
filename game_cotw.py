34187fca5beb8d612161dee3dc65be7f2e64c185
BoigahBoy <deep-noir91@hotmail.com>
2023-07-05 03:08:06 +0000

    Added base .py file
    added base file

---
+++ game_cotw.py
@@ -0,0 +1,19 @@
+
+
+from ..basic_game import BasicGame
+
+
+class CotwGame(BasicGame):
+
+    Name = "TheHunter: Call of the Wild Support Plugin"
+    Author = "ReBiRtH1991"
+    Version = "0.1.0"
+    Description = "Adds support for TheHunter: Call of the Wild"
+
+    GameName = "TheHunter: Call of the Wild"
+    GameShortName = "huntercotw"
+    GameBinary = "theHunterCotW_F.exe"
+    GameDataPath = ""
+    GameSaveExtension = "%DOCUMENTS%/Avalanche Studios/Saves/"
+    GameSteamId = 518790
+    
